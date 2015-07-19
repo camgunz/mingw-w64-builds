3b44ff2348228a54b8a3a8dcf6c5ba8cbf9a0002
Charlie Gunyon <charles.gunyon@gmail.com>
2015-07-19 16:33:11 -0400

    Add glib-static and mbed-tls build scripts; not currently working (?)

---
+++ build-glib-2.44.0-static.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+. ./config.sh
+
+./check_folders.sh
+
+./check_prereqs.sh
+
+URL="http://ftp.gnome.org/pub/gnome/sources/glib/2.44/glib-2.44.0.tar.xz"
+ARCHIVE_NAME="glib-2.44.0.tar.xz"
+TARBALL_NAME="glib-2.44.0.tar"
+SOURCE_DIR_NAME="glib-2.44.0"
+
+pushd ${ARCHIVE_DIR} > /dev/null
+${CURL} --retry 5 --remote-name -L ${URL} || exit 1
+popd > /dev/null
+
+pushd ${SOURCE_DIR} > /dev/null
+if [ -d ${SOURCE_DIR_NAME} ]
+then
+    rm -rf ${SOURCE_DIR_NAME}
+fi
+
+xz -d ${ARCHIVE_DIR}/${ARCHIVE_NAME} || exit 1
+tar xf ${ARCHIVE_DIR}/${TARBALL_NAME} || exit 1
+rm -f ${ARCHIVE_DIR}/${ARCHIVE_NAME} ${ARCHIVE_DIR}/${TARBALL_NAME} || exit 1
+
+pushd ${SOURCE_DIR_NAME} > /dev/null
+sed -i "s|#undef HAVE_IF_NAMETOINDEX|#define HAVE_IF_NAMETOINDEX 1|g" config.h.in
+./configure PYTHON="${MINGW64_DIR}/opt/bin/python" \
+            --prefix="${PREFIX}" \
+            --disable-shared \
+            --enable-static \
+            || exit 1
+
+make
+make DESTDIR=${BUILD_DIR} install
+
+popd > /dev/null
+popd > /dev/null
+
---
+++ build-mbed-tls-1.3.10.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+. ./config.sh
+
+./check_folders.sh
+
+./check_prereqs.sh
+
+URL="https://tls.mbed.org/download/start/mbedtls-1.3.10-gpl.tgz"
+ARCHIVE_NAME="mbedtls-1.3.10-gpl.tgz"
+TARBALL_NAME="mbedtls-1.3.10-gpl.tar"
+SOURCE_DIR_NAME="mbedtls-1.3.10-gpl"
+
+pushd ${ARCHIVE_DIR} > /dev/null
+${CURL} --retry 5 --remote-name -L ${URL} || exit 1
+popd > /dev/null
+
+pushd ${SOURCE_DIR} > /dev/null
+if [ -d ${SOURCE_DIR_NAME} ]
+then
+    rm -rf ${SOURCE_DIR_NAME}
+fi
+
+unzip -q ${ARCHIVE_DIR}/${ARCHIVE_NAME} || exit 1
+rm -f ${ARCHIVE_DIR}/${ARCHIVE_NAME} || exit 1
+
+pushd ${SOURCE_DIR_NAME} > /dev/null
+
+patch -p0 -f -i "${PATCH_DIR}/portmidi-217.patch"
+
+if [ -d pbuild ]
+then
+  rm -rf pbuild || exit 1
+fi
+
+mkdir pbuild || exit 1
+pushd pbuild > /dev/null
+
+cmake .. -G"Unix Makefiles" \
+         -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+         -DCMAKE_CACHEFILE_DIR="${SOURCE_DIR_NAME}/pbuild" \
+         || exit 1;
+make && \
+  cp libportmidi.dll.a libportmidi_s.a "${BUILD_DIR}/lib" &&
+  cp libportmidi.dll "${BUILD_DIR}/bin" &&
+  cp ../pm_common/portmidi.h ../porttime/porttime.h "${BUILD_DIR}/include" \
+  || exit 1
+
+popd > /dev/null
+popd > /dev/null
+popd > /dev/null
+
